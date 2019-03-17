e791782e57ab0e07b602c28905b0eb86bb2eb09f
Jalani Paul <jalani2727@gmail.com>
2019-03-17 12:36:14 -0400

    first commit

---
+++ LargerSum.py
@@ -0,0 +1,15 @@
+def larger_sum(lst1, lst2):
+    sum1 = 0
+    sum2 =0
+    for item in lst1:
+        sum1+=item
+    for item in lst2:
+        sum2+=item
+    if sum1>sum2:
+        return lst1
+    elif sum1==sum2:
+        return lst1
+    else:
+        return lst2
+
+print(larger_sum([1, 9, 5][2, 3, 7]))
